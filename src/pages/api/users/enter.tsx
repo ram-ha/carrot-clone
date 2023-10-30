81b9a5042da3e4b2e8e03119269853bbcd0a0e96
ram-ha <dlgkfka2616@gmail.com>
2023-10-30 23:56:50 +0900

    8.3 lecture

---
+++ src/pages/api/users/enter.tsx
@@ -0,0 +1,10 @@
+import client from '@/libs/server/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== 'POST') {
+        res.status(401).end();
+    }
+    console.log(req.body);
+    res.status(200).end();
+}
